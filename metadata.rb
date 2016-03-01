f23af7a1156e8b957397064eee704404d882c4a2
Tim Smith <tsmith84@gmail.com>
2016-03-01 09:46:01 -0800

    Add source/issues urls and long_desscription

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,11 @@ maintainer 'Chef Delivery Team'
 maintainer_email 'delivery-team@chef.io'
 license 'Apache 2.0'
 description 'Manages adding and removing certificates from the OS trust store'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 supports 'debian'
 supports 'ubuntu'
+
+source_url 'https://github.com/chef-cookbooks/trusted_certificate' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/trusted_certificate/issues' if respond_to?(:issues_url)
