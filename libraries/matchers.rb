2f34c192fc9106c89589afb91b4968214f24a7d5
Tim Smith <tsmith@chef.io>
2016-08-31 20:42:29 -0700

    Add Chefspec matcher
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ libraries/matchers.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: trusted_certicate
+# Library:: matchers
+#
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Copyright 2016, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if defined?(ChefSpec)
+  #################
+  # trusted_certicate
+  #################
+  ChefSpec.define_matcher :trusted_certicate
+
+  def create_trusted_certicate(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:trusted_certicate, :create, resource_name)
+  end
+end
