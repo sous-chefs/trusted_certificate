adf29927eb64fb059d52c455dc9a8fba815df174
Tim Smith <tsmith@chef.io>
2017-04-05 22:04:58 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/trusted_certificate.rb
+++ resources/trusted_certificate.rb
@@ -2,7 +2,7 @@
 # Cookbook:: trusted_certicate
 # resource:: trusted_certicate
 #
-# Copyright:: 2015-2016, Chef Software, Inc.
+# Copyright:: 2015-2017, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
