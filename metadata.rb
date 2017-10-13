f0d3167ce1ec89df183942c35c54683de70b54d5
Eduardo Lezcano Alvarez <eduardo.lezcano@be.atlascopco.com>
2017-10-13 11:31:11 +0200

    Creating certificate folder before deploying the file
    Signed-off-by: Eduardo Lezcano Alvarez <eduardo.lezcano@be.atlascopco.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Manages adding certificates to the OS trust store'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.1.1'
+version '3.1.2'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
