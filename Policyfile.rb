e989a5461208fea73cdd35dc2692368d288021d4
Collin McNeese <cmcneese@chef.io>
2020-06-19 17:38:53 -0500

    policyfile update
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- Policyfile.rb
+++ Policyfile.rb
@@ -7,7 +7,7 @@
 name 'trusted_certificate'
 
 # Where to find external cookbooks:
-default_source :chef_repo, '.'
+default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
 run_list 'trusted_certificate'
