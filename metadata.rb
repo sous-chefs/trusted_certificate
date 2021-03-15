f97d77b20ae42f0b3037e50f9192b9ddff7b83cf
Ryan <kennedy@osuosl.org>
2021-03-15 09:50:09 -0700

    platforms and ci

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,19 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Manages adding certificates to the OS trust store'
-
 version '3.3.0'
-
-%w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
-  supports os
-end
-
-source_url 'https://github.com/chef-cookbooks/trusted_certificate'
-issues_url 'https://github.com/chef-cookbooks/trusted_certificate/issues'
+source_url 'https://github.com/sous-chefs/trusted_certificate'
+issues_url 'https://github.com/sous-chefs/trusted_certificate/issues'
 chef_version '>= 12.15'
+
+supports 'ubuntu'
+supports 'debian'
+supports 'redhat'
+supports 'centos'
+supports 'suse'
+supports 'opensuse'
+supports 'opensuseleap'
+supports 'scientific'
+supports 'oracle'
+supports 'amazon'
+supports 'zlinux'
